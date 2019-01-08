5791b32813beedb31fa8dbc195996c6c98cf6836
Suqin Zhang <suqin.zhang@yitu-inc.com>
2019-01-08 11:24:26 +0800

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import Hello from './components/Hello';
+import Footer from './components/Footer';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+import { inject, observer } from 'mobx-react';
+
+interface IProps {
+  indexStore?: any
+}
+
+@inject('indexStore')
+@observer class App extends Component<IProps, {}> {
+  componentDidMount() {
+    // console.log(222);
+  }
+  
   render() {
     return (
       <div className="App">
+        <Hello title="This is a tsx title"/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -19,7 +33,15 @@ class App extends Component {
           >
             Learn React
           </a>
+          <h1>{this.props.indexStore.counter}</h1>
+          <button onClick={this.props.indexStore.addCounter}>click here</button>
+          <button onClick={()=>this.props.indexStore.clearCounter()}>clear</button>
         </header>
+        <div className="container">
+          <h1>this is a test header.</h1>
+          <p>this is description.</p>
+        </div>
+        <Footer/>
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'mobx-react';
+import {configure} from 'mobx';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import indexStore from './stores/index'
+
+configure({
+    enforceActions: "observed"
+});
+
+ReactDOM.render(
+    <Provider indexStore={indexStore}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
